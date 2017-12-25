51d0a79605b67e3625d81b262d7ebb89aaf09873
lamyaa <lamyaarizki1996@gmail.com>
2017-12-26 00:52:28 +0100

    initial commit

--- src/fichier.java
+++ src/fichier.java
@@ -1,4 +1,89 @@
-package PACKAGE_NAME;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.FileWriter;
 
 public class fichier {
+
+    public enum file_default {
+        Emploi("src/edt.csv"),
+        liste_Etudiant("src/etus.csv");
+
+        private String path;
+
+        file_default(String p){
+            this.path=p;
+        }
+        public String toString(){
+            return this.path;
+        }
+
+    }
+
+    public static String FileToString(String path) throws IOException{
+        String s="";
+       File  file= new File(path.toString());
+       Scanner inputStream =new Scanner(file);
+       while(inputStream.hasNext()){
+           String data =inputStream.next();
+           s=s.concat(data);
+       }
+       inputStream.close();
+       return s;
+    }
+
+    public static String[][] SplitFile(String path) throws Exception {
+        String s = FileToString(path.toString());
+        String[] ss = s.split("\n"); // TODO: test the \n
+        String[][] sss = new String[ss.length][];
+        for(int i = 0; i < ss.length; i++){
+            sss[i] = ss[i].split(";");
+        }
+        return sss;
+    }
+
+
+    //afficher le fichier sous sa forme originale
+    static void afficher(String path) throws IOException {
+        String s = FileToString(path.toString());
+        String[] ss = s.split("\n");// TODO: test the \n
+        for(int i = 0; i < ss.length; i++){
+            System.out.println(ss[i]+";");// TODO: delete the last character ;
+        }
+    }
+
+    static void writefile(String path) throws IOException {
+        String s="";
+        File  file= new File(path.toString());
+        System.out.println("Veuillez saisir ton emploi du temps ou t'as liste d'étudiants :");
+        Scanner inputStream =new Scanner(System.in);
+        String data =inputStream.next();
+        s=s.concat(data);
+        FileWriter f = new FileWriter(path.toString());
+        f.write(s);
+        f.close();
+        inputStream.close();
+
+
+    }
+
+    public static void main(String[] argv) throws Exception {
+    /*String s;
+    s=FileToString(fichier.file_default.Emploi.toString());
+        try {
+            String[][] tab=SplitFile(file_default.Emploi.toString());
+            for(int i=0;i<tab.length;i++) {
+                for(int j=0;j<tab[i].length;j++)
+                System.out.println(tab[i][j]);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    afficher(fichier.file_default.Emploi.toString());*/
+    writefile(fichier.file_default.Emploi.toString());
+    }
+
+
 }
--- src/menu.java
+++ src/menu.java
@@ -1,4 +1,45 @@
-package PACKAGE_NAME;
 
 public class menu {
-}
+
+        public static void main(String[] argv) throws Exception {
+            System.out.println("1. quittez l’application\n" +
+                    "2. afficher l'emlpoi du temps  \n" +
+                    "3. afficher la liste des étudiants\n" +
+                    "4. lire un fichier précis\n" +
+                    "5. créer les listes des TDs"+
+                    "6. changer le nombre maximal d'étudiants par groupe\n" +
+                    "7. saisir un nouveau emploi du temps \n"+
+                    "8. saisir une nouvelle liste d'étudiants\n" +
+                    "9. déplacer un cours ou un td");
+            int choix=0;
+            do {
+                switch (choix) {
+                    case 1:
+                        System.exit(0);
+                    case 2:
+
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                    case 9:
+                        break;
+                }
+            }while(choix!=1);
+
+
+
+
+
+        }}
+
+
